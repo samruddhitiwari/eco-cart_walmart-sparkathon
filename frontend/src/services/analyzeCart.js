0d82d680e40dbc9c50035818f1e6fd774d010a92
samruddhitiwari <121943400+samruddhitiwari@users.noreply.github.com>
2025-07-13 21:52:48 +0530

    Update analyzeCart.js

--- frontend/src/services/analyzeCart.js
+++ frontend/src/services/analyzeCart.js
@@ -1,8 +1,7 @@
-// services/analyzeCart.js
 import api from "./api";
 
 export async function analyzeCart(cart) {
-  const itemIds = cart.map(item => item.product_name); // use correct key
+  const itemIds = cart.map(item => item.name.toLowerCase().replace(/ /g, "_"));
 
   try {
     const response = await api.post("/analyze-cart", {
